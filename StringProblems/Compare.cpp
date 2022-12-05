16107e7214ea62f2752af424e3d8abe2d29ea373
GhazalDaoun <ghazaldaoun@gmail.com>
2022-12-05 10:00:45 +0200

    ST

---
+++ StringProblems/Compare.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string value1,value2;
+    cin>>value1;
+    cin>>value2;
+    if(value1 < value2){
+        cout<<value1;
+    }else {
+    cout<<value2;
+    }
+}
