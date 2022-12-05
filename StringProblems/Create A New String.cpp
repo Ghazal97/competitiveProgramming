cf781b5d53dce903dede5e315e4827960ce97c99
GhazalDaoun <ghazaldaoun@gmail.com>
2022-12-05 10:16:28 +0200

    ST

---
+++ StringProblems/Create A New String.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    cout<<s1.size()<<" "<<s2.size()<<endl;
+    cout<<s1<<" "<<s2<<endl;
+
+
+}
